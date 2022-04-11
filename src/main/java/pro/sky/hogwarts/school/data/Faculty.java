e0fd025e947ffe130f4e5286b1c5306d631eb493
pro100 <pro100tkach@mail.ru>
2022-04-11 21:44:42 +0300

    lesson 3.1 done

--- src/main/java/pro/sky/hogwarts/school/data/Faculty.java
+++ src/main/java/pro/sky/hogwarts/school/data/Faculty.java
@@ -1,34 +1,34 @@
-//package pro.sky.hogwarts.school.data;
-//
-//public class Faculty {
-//    private final Long id;
-//    private final String name;
-//    private final String color;
-//
-//    public Faculty(Long id, String name, String color) {
-//        this.id = id;
-//        this.name = name;
-//        this.color = color;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getColor() {
-//        return color;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Faculty{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", color='" + color + '\'' +
-//                '}';
-//    }
-//}
+package pro.sky.hogwarts.school.data;
+
+public class Faculty {
+    private final Long id;
+    private final String name;
+    private final String color;
+
+    public Faculty(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
--- src/main/java/pro/sky/hogwarts/school/data/Student.java
+++ src/main/java/pro/sky/hogwarts/school/data/Student.java
@@ -1,57 +1,57 @@
-//package pro.sky.hogwarts.school.data;
-//
-//import java.util.Objects;
-//
-//public class Student {
-//    private final Long id;
-//    private String name;
-//    private int age;
-//
-//    public Student(Long id, String name, int age) {
-//        this.id = id;
-//        this.name = name;
-//        this.age = age;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Student student = (Student) o;
-//        return Objects.equals(name, student.name);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(name);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Student{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", age=" + age +
-//                '}';
-//    }
-//}
+package pro.sky.hogwarts.school.data;
+
+import java.util.Objects;
+
+public class Student {
+    private final Long id;
+    private String name;
+    private int age;
+
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
